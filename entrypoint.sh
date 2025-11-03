3c13335cbede24993d444fe4589d133275a74ea2
Moritz <moritz.walter.hy@gmail.com>
2025-11-03 13:10:15 +0100

    initial

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Ensure logs directory exists
 mkdir -p /home/container/logs
+mkdir -p /run/php
+mkdir -p /run/nginx
+
+chown -R www-data:www-data /home/container
 
 # Start supervisor with your config
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
