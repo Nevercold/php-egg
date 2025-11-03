8b832c73b72f22c30e3f2035199b7db13670269e
Moritz <moritz.walter.hy@gmail.com>
2025-11-03 13:13:16 +0100

    initial

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Ensure logs directory exists
 mkdir -p /home/container/logs
-mkdir -p /run/php
-mkdir -p /run/nginx
+mkdir -p /home/container/run/php
+mkdir -p /home/container/run/nginx
 
-chown -R www-data:www-data /home/container
+sed -i 's|/run/php/php8.4-fpm.sock|/home/container/run/php/php8.4-fpm.sock|g' /etc/php/8.4/fpm/pool.d/www.conf
+
 
 # Start supervisor with your config
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
